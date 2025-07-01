5d223b41835515f9d47912828ef8f5fa28373fd7
Cristian Nahuas <crnahuas@gmail.com>
2025-06-30 21:24:51 -0400

    ComicShopApp Do while

--- ComicShopApp/src/main/java/com/crnahuas/comicshopapp/ComicShopApp.java
+++ ComicShopApp/src/main/java/com/crnahuas/comicshopapp/ComicShopApp.java
@@ -9,8 +9,9 @@ import java.util.Scanner;
 import java.util.TreeSet;
 
 // Sistema principal que administra la tienda de cómics y usuarios.
-//Utiliza colecciones, manejo de archivos y validaciones para una gestión robusta.
+// Utiliza colecciones, manejo de archivos y validaciones para una gestión robusta.
 public class ComicShopApp {
+
     // Almacena todos los cómics disponibles.
     static ArrayList<Comics> comicsDisponibles = new ArrayList<>();
     // Asocia RUT con los objetos Usuario.
@@ -21,9 +22,45 @@ public class ComicShopApp {
     static TreeSet<String> usuariosOrdenados = new TreeSet<>();
     // Lectura desde consola.
     static Scanner sc = new Scanner(System.in);
-    
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int opcion = 0;
+
+        // Bucle principal del menú
+        do {
+            mostrarMenu();
+            try {
+                opcion = Integer.parseInt(sc.nextLine().trim());
+
+                switch (opcion) {
+                    case 1 ->
+                        cargarComicsDesdeArchivo("comics.csv");
+                    case 2 ->
+                        registrarUsuario();
+                    case 3 ->
+                        agregarCompraUsuario();
+                    case 4 ->
+                        guardarUsuarios("usuarios.txt");
+                    case 5 ->
+                        mostrarComicsDisponibles();
+                    case 6 ->
+                        eliminarUsuario();
+                    case 7 ->
+                        eliminarComic();
+                    case 8 ->
+                        exportarUsuariosCSV("usuarios.csv");
+                    case 9 ->
+                        System.out.println("\nGracias por usar ComicCollectorSystem. ¡Hasta la próxima!");
+                    default ->
+                        System.out.println("\nOpción inválida. Intente nuevamente.");
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println("\nDebe ingresar un número válido. Intente nuevamente.");
+            } catch (Exception e) {
+                System.out.println("\nOcurrió un error inesperado: " + e.getMessage());
+            }
+        } while (opcion != 9);
+
     }
-    
 }
