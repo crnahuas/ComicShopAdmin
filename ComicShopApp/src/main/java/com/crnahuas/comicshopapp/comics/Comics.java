0d5f3c3c4390c256c48878e0bbc586585d1d38a2
Cristian Nahuas <crnahuas@gmail.com>
2025-06-30 20:50:43 -0400

    Creación Comics.java Constructor, Getters & Setters, toString + Comentarios

--- ComicShopApp/src/main/java/com/crnahuas/comicshopapp/comics/Comics.java
+++ ComicShopApp/src/main/java/com/crnahuas/comicshopapp/comics/Comics.java
@@ -2,5 +2,61 @@ package com.crnahuas.comicshopapp.comics;
 
 //Clase que representa un cómic en la tienda con sus atributos principales.
 public class Comics {
+    private String codigo;
+    private String titulo;
+    private String autor;
+    private double precio;
+
+    // Constructor vacio.
+    public Comics() {
+    }
     
+    // Constructor que inicializa todos los campos del cómic.
+    public Comics(String codigo, String titulo, String autor, double precio) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.precio = precio;
+    }
+
+     // Métodos getter.
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    // Métodos setter.
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    // Representación legible del cómic para mostrar o exportar.
+    @Override
+    public String toString() {
+        return "[" + codigo + "] " + titulo + " - " + autor + " ($" + precio + ")";
+    }
+ 
 }
